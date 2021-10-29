37a98d6d89441d3f91ebb92abf4c81c252a2d3d5
battatotal <91632132+battatotal@users.noreply.github.com>
2021-10-29 13:35:51 +0300

    Update task2.py

--- task2/task2.py
+++ task2/task2.py
@@ -2,24 +2,23 @@ from sys import argv
 import math
 
 
-class PointNumbersError(Exception):
-    pass
+#class PointNumbersError(Exception):
+    #pass
 
 with open(argv[1], encoding='UTF-8') as file:
     mid = list(map(float, file.readlines(1)[0].replace("\n", "").split()))
     rad = float(file.readlines(2)[0].replace("\n", ""))
 
-try:
-    with open(argv[2], encoding='UTF-8') as file:
-        points = [x.split() for x in file.readlines()]
-        if len(points) > 100 or len(points) == 0:
-            raise PointNumbersError
+
+    
+with open(argv[2], encoding='UTF-8') as file:
+    points = [x.split() for x in file.readlines()]
+    if len(points) < 101 or len(points) != 0:
         for i in points:
             for j in range(len(i)):
                 i[j] = float(i[j])
 
-except:
-    print("Ошибка: количество точек должно быть от 1 до 100")
+
 
 
 def distance(point1, point2):
@@ -35,6 +34,26 @@ for i in range(len(points)):
     print(in_circle(points[i]))
 
 
-    
+ 
+
+
+#try:
+    #with open(argv[2], encoding='UTF-8') as file:
+        #points = [x.split() for x in file.readlines()]
+        #if len(points) > 100 or len(points) == 0:
+            #raise PointNumbersError
+        #for i in points:
+            #for j in range(len(i)):
+                #i[j] = float(i[j])
+
+#except:
+    #print("Ошибка: количество точек должно быть от 1 до 100")
+
+
+
+
+
+
+
     
     
